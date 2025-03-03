53dd12e8c47be5f352c2f979ac47a5aef9a1e4c0
mandlmayr <123469781+mandlmayr@users.noreply.github.com>
2025-03-03 23:40:54 +0100

    Update opti.py

--- opti.py
+++ opti.py
@@ -4,6 +4,7 @@ Created on Mon Feb 24 20:32:00 2025
 
 @author: Michael
 """
+import numpy as np
 
 class Opt:
     def __init__(self):   
@@ -29,3 +30,47 @@ class Opt:
         self.nonlinear_constraint_name.append(name)
         self.nonlinear_constraint_size.append(size)
         
+    def setupIndices(self):
+        self.ind_var=[0]
+        self.ind_linear_constraint=[0]
+        self.ind_nonlinear_constraint=[0]
+        
+        size_counter=0
+        for size in self.variable_size:
+            size_counter+=size
+            self.ind_var.append(size_counter)
+            
+        size_counter=0
+        for size in self.linear_constraint_size:
+            size_counter+=size
+            self.ind_linear_constraint.append(size_counter)
+            
+        size_counter=0
+        for size in self.nonlinear_constraint_size:
+            size_counter+=size
+            self.ind_nonlinear_constraint.append(size_counter)
+            
+    def indexVar(self,var_name):
+        ind=self.variable_name.index(var_name)
+        return self.ind_var[ind], self.ind_var[ind+1]
+        
+    def indexLinConstr(self, constr_name):
+        ind=self.linear_constraint_name.index(constr_name)
+        return self.ind_linear_constraint[ind], self.ind_linear_constraint[ind+1]
+    
+    def indexNonlinConstr(self, constr_name):
+        ind=self.nonlinear_constraint_name.index(constr_name)
+        return self.ind_nonlinear_constraint[ind], self.ind_nonlinear_constraint[ind+1]
+        
+            
+            
+        
+        
+        
+        
+p1=Opt()
+p1.addVar("x1", 10)
+p1.addVar("x2",15)
+p1.addLinConstr("bilanz1", 20)
+p1.addLinConstr("bilanz2", 15)
+p1.setupIndices()
